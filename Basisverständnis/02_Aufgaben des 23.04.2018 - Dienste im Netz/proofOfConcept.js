c4418fdec6e8ea0c29400015684bae3c68bc4085
sgeier96 <Geier_Stefan@gmx.de>
2018-05-29 21:27:12 +0200

    PoC erweitert und .gitignore erstellt

--- Basisverständnis/02_Aufgaben des 23.04.2018 - Dienste im Netz/proofOfConcept.js
+++ Basisverständnis/02_Aufgaben des 23.04.2018 - Dienste im Netz/proofOfConcept.js
@@ -1,7 +1,11 @@
 var fs = require('fs');                                                         //I/O-functions
 var express = require("express");                                               //app.get, etc.
 var request = require("request");                                               //request(url, function(err, res, body))), etc.
-//var bodyParser = require("bodyParser");                                         //parser for i.e. JSON
+//var bodyParser = require("bodyParser");                                       //parser for i.e. JSON
+var stdin = process.openStdin();
+var stdio = require('stdio');
+var readlineSync = require('readline-sync');
+
 
 var app = express();
 var result;
@@ -13,12 +17,21 @@ const settings = {
 
 var okta = {
   staticURLPart: "https://dev-989237.oktapreview.com/api/v1/",
-  apikey: "000mGb4MnfuqumngmGcxL1lx_-wx8LkcFY_1Jw6EZC"
+  api_token: '00fcqg4Dh2qmAr-SVcLT9Zxwq48i3xuyWhWNJrMZzX'
   //...
 }
 
 //---------------------JUST A PLAYGROUND-----------------------
 
+stdin.addListener("data", function(d){
+  //note: d is an object, and when converted to a string it will
+  //end with a linefeed. So we (rather crudely) account for that
+  //with toString() and them trim()
+  let latestInput = d.toString().trim();
+  processConsoleInput(latestInput);
+});
+
+/*
 var user =
 '{"username": "DaveDaveson@webmail.com",'+
 '"password": "62iLfVqn"}';
@@ -35,11 +48,12 @@ var options = {
 }
 
 request.post(options, function(error, res, body){
-  console.log("error: ", error); //Print the error if one occured
-  console.log("statusCode: ", res && res.statusCode); //Print the response status code if a response was received
-  console.log(body);
+  console.log("error: ", error);                                                //Print the error if one occured
+  console.log("statusCode: ", res && res.statusCode);                           //Print the response status code if a response was received
+  console.log("\n");
+  console.log(JSON.parse(body));
 });
-
+*/
 
 //--------------------END OF PLAYGROUND------------------------
 
@@ -51,6 +65,87 @@ app.post('/*', function(req, res){
   response.send(result);
 });
 
+function processConsoleInput (latestInput){
+  switch (latestInput) {
+    case "login":
+          let username = readlineSync.question('Username/Email address: ');
+          let password = readlineSync.question('Password: ', {
+            hideEchoBack: true
+          });
+
+          var options = {
+            url: okta.staticURLPart + "/authn",
+            body: '{"username": "' + username + '",'+
+            '"password": "' + password + '"}',
+            headers: {
+              'Accept' : 'application/json',
+              'Content-Type' : 'application/json'
+            }
+          }
+
+          request.post(options, function(error, res, body){
+            console.log("error: ", error);                                      //Print the error if one occured
+            console.log("statusCode: ", res && res.statusCode);                 //Print the response status code if a response was received
+            console.log("\n");
+            console.log(JSON.parse(body));
+          });
+          break;
+    case "register":
+          let firstName = readlineSync.question('first name: ');
+          let lastName = readlineSync.question('last name: ');
+          let prefEmail = readlineSync.question('email: ');
+          //let mobilePhone = readlineSync.question('mobile phone: ');
+          let prefPassword = readlineSync.question('password: ');
+
+          var options = {
+            url: okta.staticURLPart + "/users",                                 //?activate=false preferably, but in our case not necessary
+            body: '{ "profile": { "firstName": "' + firstName +
+            '", "lastName": "' + lastName +
+            '", "email": "' + prefEmail +
+            '", "login": "' + prefEmail +
+            //'", "mobilePhone": "' + mobilePhone +
+            '"}, "credentials": { "password": {"value": "' + prefPassword +
+            '"}}}',
+            headers: {
+              'Accept' : 'application/json',
+              'Content-Type' : 'application/json',
+              'Authorization' : 'SSWS ' + okta.api_token
+            }
+          }
+
+          request.post(options, function(error, res, body){
+            console.log("error: ", error);
+            console.log("statusCode: ", res && res.statusCode);
+            console.log("\n");
+            console.log(JSON.parse(body));
+          });
+
+      break;
+    case "create group":
+
+          let groupName = readlineSync.question('Preferred group name: ');
+          let groupDescription = readlineSync.question('Preferred group description: ');
+
+          var options = {
+            url: okta.staticURLPart + "/groups",
+            body: '{ "profile": { "name": "' + groupName +
+            '", "description": "' + groupDescription + '"}}',
+            headers: {
+              'Accept' : 'application/json',
+              'Content-Type' : 'application/json',
+              'Authorization' : 'SSWS ' + okta.api_token
+            }
+          }
+
+          request.post(options, function(error, res, body){
+            console.log("error: ", error);
+            console.log("statusCode: ", res && res.statusCode);
+            console.log("\n");
+            console.log(JSON.parse(body));
+          });
+    break;
+  }
+}
 
 app.listen(settings.port, function(){
   console.log("'Dienstgeber' now running on port " + settings.port);
